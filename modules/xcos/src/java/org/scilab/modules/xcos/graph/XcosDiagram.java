c56ab9f79794b9680afd799d9a71eb9c53b5c193
Sunil Shetye <sunilshetye@rocketmail.com>
2023-08-27 17:02:31 +0530

    set year to 2023

--- modules/xcos/src/java/org/scilab/modules/xcos/graph/XcosDiagram.java
+++ modules/xcos/src/java/org/scilab/modules/xcos/graph/XcosDiagram.java
@@ -4,7 +4,7 @@
  * Copyright (C) 2009-2010 - DIGITEO - Clement DAVID
  * Copyright (C) 2011-2017 - Scilab Enterprises - Clement DAVID
  * Copyright (C) 2015 - Marcos Cardinot
- * Copyright (C) 2017-2019 - ESI Group - Clement DAVID
+ * Copyright (C) 2017-2023 - ESI Group - Clement DAVID
  *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
  *
